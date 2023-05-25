fbab02eb4eafcd228da1642a04da8a804e1fdd82
Adam Rogowski <86149@student.pb.edu.pl>
2023-05-25 11:47:59 +0200

    First go on implementing MobileRadarObject

--- src/radar_objects/MobileRadarObject.hpp
+++ src/radar_objects/MobileRadarObject.hpp
@@ -12,7 +12,8 @@ class MobileRadarObject : public RadarObject {
         : RadarObject(position), m_route(route) {}
     Route& route() { return m_route; }
     void change_route(Route new_route) { m_route = new_route; }
-    virtual RealPosition position_after();
+    virtual RealPosition position_after(double delta_time);
     virtual void process(double delta_time);
     double heading();
+    double time_to_checkpoint() const;
 };
