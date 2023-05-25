4a468ab316aa54c3162f28771cf58dc909977046
icebox246 <iceboxpoczta@gmail.com>
2023-05-25 21:21:56 +0200

    Introduce examples of all immobile objects
    This is a temporary hack to just test out different object types.

--- src/RadarSystem.cpp
+++ src/RadarSystem.cpp
@@ -3,14 +3,21 @@
 #include "radar_objects/Glider.hpp"
 #include "radar_objects/Helicopter.hpp"
 #include "radar_objects/HotAirBalloon.hpp"
+#include "radar_objects/Mountain.hpp"
 #include "radar_objects/Plane.hpp"
 #include "radar_objects/Route.hpp"
 #include "radar_objects/Skyscraper.hpp"
+#include "radar_objects/Windmill.hpp"
 #include "util.hpp"
 
 RadarSystem::RadarSystem() {
     // TODO: don't do that
-    auto building = std::make_unique<Skyscraper>(RealPosition(1, 2), 32);
+    std::unique_ptr<RadarObject> building =
+        std::make_unique<Skyscraper>(RealPosition(2, 5), 32);
+    m_radar_objects.push_back(std::move(building));
+    building = std::make_unique<Mountain>(RealPosition(4, 10), 32);
+    m_radar_objects.push_back(std::move(building));
+    building = std::make_unique<Windmill>(RealPosition(6, 15), 32);
     m_radar_objects.push_back(std::move(building));
 
     for (auto i = 0; i < 10; i++) {
