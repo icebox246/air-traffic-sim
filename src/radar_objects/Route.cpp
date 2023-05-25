a4d0a0af8dd00a0ecaf76440139cb6e2a1ce1998
Adam Rogowski <86149@student.pb.edu.pl>
2023-05-25 11:00:20 +0200

    First pass of implementing Route, introduced RouteCheckpoint

---
+++ src/radar_objects/Route.cpp
@@ -0,0 +1,11 @@
+#include "Route.hpp"
+
+#include "../util.hpp"
+
+double Route::altitude() const { TODO(); }
+
+double Route::velocity() const { TODO(); }
+
+RealPosition Route::target() const { TODO(); };
+
+bool Route::advance_checkpoint() { TODO(); };
---
+++ src/radar_objects/RouteCheckpoint.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+class RouteCheckpoint {
+    // TODO: implement
+};
