cd235e65d12ebabdaec5fa0ba4f7d2e94de709cb
Adam Rogowski <86149@student.pb.edu.pl>
2023-05-25 12:08:40 +0200

    Implement upper and lower _bound for MobileRadarObject

--- src/radar_objects/MobileRadarObject.hpp
+++ src/radar_objects/MobileRadarObject.hpp
@@ -13,6 +13,8 @@ class MobileRadarObject : public RadarObject {
     Route& route() { return m_route; }
     void change_route(Route new_route) { m_route = new_route; }
     virtual RealPosition position_after(double delta_time);
+    virtual double upper_bound() { return m_route.altitude() + m_radius; }
+    virtual double lower_bound() { return m_route.altitude() - m_radius; }
     virtual void process(double delta_time);
     double heading();
     double time_to_checkpoint() const;
