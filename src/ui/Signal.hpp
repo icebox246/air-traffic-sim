a32eff9bd0ee421e02a34a1b1218728a77a9c62b
Adam Rogowski <86149@student.pb.edu.pl>
2023-05-25 12:31:20 +0200

    Add better handing for void(...) signals

--- src/ui/Signal.hpp
+++ src/ui/Signal.hpp
@@ -11,6 +11,23 @@ class Signal {
     void connect(std::function<rT(aTs...)> callback) { m_callback = callback; }
 
     rT call(aTs... args) {
-        if (m_callback) return m_callback(args...);
+        if (m_callback)
+            return m_callback(args...);
+        else
+            return {};
+    }
+};
+
+template <typename... aTs>
+class Signal<void, aTs...> {
+   protected:
+    std::function<void(aTs...)> m_callback;
+
+   public:
+    void connect(std::function<void(aTs...)> callback) { m_callback = callback; }
+
+    void call(aTs... args) {
+        if (m_callback)
+            return m_callback(args...);
     }
 };
