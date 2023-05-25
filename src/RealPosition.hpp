d8340fa87e78f834d89e7490e9e92f976b2e0c70
Adam Rogowski <86149@student.pb.edu.pl>
2023-05-25 11:23:23 +0200

    Make Position more const correct

--- src/RealPosition.hpp
+++ src/RealPosition.hpp
@@ -8,7 +8,7 @@ class RealPosition : public Position<double> {
    public:
     RealPosition() : Position<double>(0, 0) {}
     RealPosition(double x, double y) : Position<double>(x, y) {}
-    double distance_from(RealPosition other) {
+    double distance_from(RealPosition other) const {
         double dx = x() - other.x();
         double dy = y() - other.y();
         return sqrt(dx * dx + dy * dy);
