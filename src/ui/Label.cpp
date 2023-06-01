4816f16c7a49f9321c011720eddc8b6ce1f89c13
icebox246 <iceboxpoczta@gmail.com>
2023-06-01 08:22:47 +0200

    Change Label to not use GuiLabel
    This fixes problems with wrong font color, as it seemed to be impossible
    to change color of raygui label.

--- src/ui/Label.cpp
+++ src/ui/Label.cpp
@@ -9,4 +9,13 @@ Label::Label(int x, int y, int width, int height, std::string text)
 }
 void Label::set_text(std::string new_text) { m_text = std::move(new_text); }
 
-void Label::process() { GuiLabel(m_bounds, m_text.c_str()); }
+void Label::process() {
+    Vector2 position;
+    position.x = m_bounds.x;
+    position.y =
+        m_bounds.y + (m_bounds.height - GuiGetStyle(DEFAULT, TEXT_SIZE)) / 2;
+    DrawTextEx(GuiGetFont(), m_text.c_str(), position,
+               GuiGetStyle(DEFAULT, TEXT_SIZE),
+               GuiGetStyle(DEFAULT, TEXT_SPACING),
+               GetColor(GuiGetStyle(DEFAULT, TEXT_COLOR_NORMAL)));
+}
