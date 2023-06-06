d10ffbcf76082de919b4eaf132aa3cc55de7ffdd
icebox246 <iceboxpoczta@gmail.com>
2023-06-06 19:52:30 +0200

    Replace ASSERT(...) with exceptions in Route

--- src/radar_objects/Route.cpp
+++ src/radar_objects/Route.cpp
@@ -3,22 +3,23 @@
 #include "../util.hpp"
 
 double Route::altitude() const {
-    ASSERT(!m_checkpoints.empty());
+    if (m_checkpoints.empty()) throw EmptyCheckpointsListException("altitude");
     return m_checkpoints.front().altitude();
 }
 
 double Route::velocity() const {
-    ASSERT(!m_checkpoints.empty());
+    if (m_checkpoints.empty()) throw EmptyCheckpointsListException("velocity");
     return m_checkpoints.front().velocity();
 }
 
 RealPosition Route::target() const {
-    ASSERT(!m_checkpoints.empty());
+    if (m_checkpoints.empty()) throw EmptyCheckpointsListException("target");
     return m_checkpoints.front().point();
 };
 
 bool Route::advance_checkpoint() {
-    ASSERT(!m_checkpoints.empty());
+    if (m_checkpoints.empty())
+        throw EmptyCheckpointsListException("advance_checkpoint");
     m_checkpoints.erase(m_checkpoints.cbegin());
     return !m_checkpoints.empty();
 };
